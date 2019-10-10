36676445375f8bbe0f572d96025cb5c674614b35
cattre <cattre@users.noreply.github.com>
2019-10-10 21:04:09 +0000

    automated commit by check50 [check50=True]

---
+++ cash.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+float gpf(string prompt);
+
+int main(void)
+{
+    float dollars = gpf("Change owed:");
+    int cents = round(dollars*100);
+    int q = cents/25; // Number of quarters
+    int d = (cents%25)/10; // Number of dimes
+    int n = ((cents%25)%10)/5; // Number of nickels
+    int c = (((cents%25)%10)%5);
+    printf("%i\n",q+d+n+c);
+}
+
+float gpf(string prompt)
+{
+    float f;
+    do
+    {
+        f = get_float("%s",prompt);
+    }
+    while (f<0.01);
+    return f;
+}
