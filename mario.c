9ca2a711e8a9c0f79f48501c317fbfcc2e060ed0
cattre <cattre@users.noreply.github.com>
2020-05-03 20:03:51 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -1,26 +1,33 @@
-// Build a right aligned half pyramid
+// Build two sided pyramid with gap
 
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
     int height = 0;
-    // Prompts user until integer between 1 and 8 entered
+    // Checks for correct user input
     while (height < 1 || height > 8)
     {
         height = get_int("Height:");
     }
-    // Iterates through rows
+    // Iterates over rows
     for (int i = 0; i < height; i++)
     {
-        // Prints leading spaces based on row number
+        // Prints leading spaces based on row
         for (int j = 0; j < height - i - 1; j++)
         {
             printf(" ");
         }
-        // Prints hashes based on row number
+        // Prints left hand hashes based on row
         for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        // Prints right hand hashes based on row
+        for (int l = 0; l < i + 1; l++)
         {
             printf("#");
         }
