fef07cb745bfff8c077106eebda6d0b77ffc354c
cattre <cattre@users.noreply.github.com>
2020-11-07 16:57:14 +0000

    automated commit by check50 [check50=True]

---
+++ recover.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    // Check for proper usage
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+
+    // Open raw data file
+    FILE *rawfile = fopen(argv[1], "r");
+    if (rawfile == NULL)
+    {
+        printf("Unable to open file\n");
+        return 1;
+    }
+
+    FILE *newfile;
+    char filename[16];
+    int filecount = 0;
+    unsigned char bytes[512];
+
+    while (true)
+    {
+
+        if (feof(rawfile))
+        {
+            fclose(rawfile);
+            break;
+        }
+
+        // Read 512 bytes from file
+        fread(bytes, 512, 1, rawfile);
+
+        // Check if start of new jpg
+        if (bytes[0] == 0xff &&
+            bytes[1] == 0xd8 &&
+            bytes[2] == 0xff &&
+            (bytes[3] & 0xf0) == 0xe0)
+        {
+            // Check if first jpg
+            if (filecount == 0)
+            {
+                // strcpy(filename, "000.jpg");
+                // Update filename
+                sprintf(filename, "000.jpg");
+
+                // Open new file for writing using filename
+                newfile = fopen(filename, "w");
+
+                // Print bytes to file
+                // fprintf(newfile, "%s", bytes);
+                fwrite(bytes, sizeof(bytes), 1, newfile);
+
+                // Close file
+                fclose(newfile);
+
+                // Increment filecount
+                filecount++;
+                // printf("one");
+            }
+            else
+            {
+                // Update filename
+                sprintf(filename, "%03i.jpg", filecount);
+
+                // Open new file for writing using new filename
+                newfile = fopen(filename, "w");
+
+                // Print bytes to file
+                fwrite(bytes, sizeof(bytes), 1, newfile);
+
+                // Close file
+                fclose(newfile);
+
+                // Increment filecount
+                filecount++;
+                // printf("two");
+            }
+        }
+        // If not at the start of a file
+        else
+        {
+            // If a file has already started
+            if (filecount > 0)
+            {
+                // Open new file for appending using new filename
+                newfile = fopen(filename, "a");
+    
+                // Print bytes to file
+                fwrite(bytes, sizeof(bytes), 1, newfile);
+                // printf("yay\n");
+                // Close file
+                fclose(newfile);
+            }
+        }
+    }
+
+    fclose(rawfile);
+    return 0;
+}
